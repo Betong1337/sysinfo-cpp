5c78d4643285e4ac5cc1ddf82ae3dd9d77e0537b
Betong1337 <asp.jakob@outlook.com>
2026-02-26 23:02:54 +0100

    Fixed a bug when lspci was not installed

--- src/gpu/info.h
+++ src/gpu/info.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include "core/core.h"
 
-#define GPU_COMMAND "/bin/lspci | grep -iE 'VGA|3D|video'"
+#define GPU_COMMAND "(/bin/lspci | grep -iE 'VGA|3D|video') 2>/dev/null"
 #define GPU_PREFIX "GPU: "
 
 InfoEntry parse_gpu();
