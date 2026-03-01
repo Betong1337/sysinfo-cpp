a18599ec943c57bc9b71b065a72a1a8fc0a60dbe
Betong1337 <asp.jakob@outlook.com>
2026-03-01 17:16:31 +0100

    fixed disk

--- src/platform/windows/disk/info.cpp
+++ src/platform/windows/disk/info.cpp
@@ -14,28 +14,27 @@ InfoEntry parse_disk() {
     ostringstream oss;
     result.prefix = DISK_PREFIX;
 
-    ULARGE_INTEGER freeBytesAvailable;
-    ULARGE_INTEGER totalNumberOfBytes;
-    ULARGE_INTEGER totalNumberOfFreeBytes;
+    ULARGE_INTEGER totalBytes;
+    ULARGE_INTEGER freeBytes;
 
     if (GetDiskFreeSpaceExA(
             "C:\\",
-            &freeBytesAvailable,
-            &totalNumberOfBytes,
-            &totalNumberOfFreeBytes))
+            nullptr,
+            &totalBytes,
+            &freeBytes))
     {
+        unsigned long long usedBytes =
+            totalBytes.QuadPart - freeBytes.QuadPart;
+
         unsigned long long totalGB =
-            totalNumberOfBytes.QuadPart / (1024ULL * 1024ULL * 1024ULL);
+            totalBytes.QuadPart / (1024ULL * 1024ULL * 1024ULL);
 
-        unsigned long long freeGB =
-            totalNumberOfFreeBytes.QuadPart / (1024ULL * 1024ULL * 1024ULL);
-
-        unsigned long long usedGB = totalGB - freeGB;
+        unsigned long long usedGB =
+            usedBytes / (1024ULL * 1024ULL * 1024ULL);
 
         int percent = static_cast<int>(
-            (double)(totalBytes.QuadPart - freeBytes.QuadPart)
-            / totalBytes.QuadPart * 100.0
-        ); 
+            (double)usedBytes / totalBytes.QuadPart * 100.0
+        );
         
         const char* percentage_color = get_percentage_color(percent);
 
